374afe05884b1f416e83ac9e8017cc76f6498866
Xackery <xackery@gmail.com>
2025-03-27 16:49:14 -0700

    Add debug option for decoder

--- decoder.go
+++ decoder.go
@@ -1,17 +1,21 @@
 package encdec
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"io"
+	"math"
 )
 
 // Decoder is struct for decoding data.
 type Decoder struct {
-	order      binary.ByteOrder
-	r          io.ReadSeeker
-	firstError error
-	lastError  error
+	order       binary.ByteOrder
+	r           io.ReadSeeker
+	firstError  error
+	lastError   error
+	isDebugMode bool
+	debugBuf    bytes.Buffer
 }
 
 // NewDecoder returns new Decoder.
@@ -20,6 +24,31 @@ func NewDecoder(r io.ReadSeeker, order binary.ByteOrder) *Decoder {
 		order: order,
 		r:     r,
 	}
+}
+
+// SetDebugMode enables every decode call to write to a stored buffer in the decoder to review later
+func (d *Decoder) SetDebugMode(value bool) {
+	d.isDebugMode = value
+}
+
+// DebugBuf returns the debug buffer
+func (d *Decoder) DebugBuf() []byte {
+	return d.debugBuf.Bytes()
+}
+
+// DebugString returns the debug buffer as a string
+func (d *Decoder) DebugString() string {
+	return string(d.debugBuf.Bytes())
+}
+
+// DebugClear clears the debug buffer
+func (d *Decoder) DebugClear() {
+	d.debugBuf.Reset()
+}
+
+// IsDebugMode returns if debug mode is enabled
+func (d *Decoder) IsDebugMode() bool {
+	return d.isDebugMode
 }
 
 // SetOrder sets byte order.
@@ -65,29 +94,55 @@ func (d *Decoder) Bytes(n int) []byte {
 
 // Byte returns byte.
 func (d *Decoder) Byte() byte {
+	if d.isDebugMode {
+		value := d.Bytes(1)[0]
+		d.debugBuf.Write([]byte{value})
+		return value
+	}
 	return d.Bytes(1)[0]
 }
 
 // StringFixed returns fixed string.
 func (d *Decoder) StringFixed(n int) string {
+	if d.isDebugMode {
+		value := string(d.Bytes(n))
+		d.debugBuf.Write([]byte(value))
+		return value
+	}
 	return string(d.Bytes(n))
 }
 
 // StringLenPrefixUint32 returns string with length prefix assumed to be prior
 func (d *Decoder) StringLenPrefixUint32() string {
 	n := d.Uint32()
+	if d.isDebugMode {
+		value := d.StringFixed(int(n))
+		d.debugBuf.Write([]byte(value))
+		return value
+	}
 	return d.StringFixed(int(n))
 }
 
 // StringLenPrefixUint16 returns string with length prefix assumed to be prior
 func (d *Decoder) StringLenPrefixUint16() string {
 	n := d.Uint16()
+	if d.isDebugMode {
+		value := d.StringFixed(int(n))
+		d.debugBuf.Write([]byte(value))
+		return value
+	}
 	return d.StringFixed(int(n))
 }
 
 // StringLenPrefixUint8 returns string with length prefix assumed to be prior
 func (d *Decoder) StringLenPrefixUint8() string {
 	n := d.Uint8()
+	if d.isDebugMode {
+		value := d.StringFixed(int(n))
+		d.debugBuf.Write([]byte(value))
+		return value
+	}
+
 	return d.StringFixed(int(n))
 }
 
@@ -114,11 +169,21 @@ func (d *Decoder) StringZero() string {
 		}
 		s += string(buf[:])
 	}
+	if d.isDebugMode {
+		d.debugBuf.Write([]byte(s))
+	}
+
 	return s
 }
 
 // Bool returns bool.
 func (d *Decoder) Bool() bool {
+	if d.isDebugMode {
+		value := d.Byte() != 0
+		d.debugBuf.Write([]byte{0})
+		return value
+	}
+
 	return d.Byte() != 0
 }
 
@@ -136,6 +201,9 @@ func (d *Decoder) Uint8() uint8 {
 			d.firstError = d.lastError
 		}
 	}
+	if d.isDebugMode {
+		d.debugBuf.Write([]byte{v})
+	}
 	return v
 }
 
@@ -152,6 +220,10 @@ func (d *Decoder) Uint16() uint16 {
 		if d.firstError != nil {
 			d.firstError = d.lastError
 		}
+	}
+
+	if d.isDebugMode {
+		d.debugBuf.Write([]byte{byte(v >> 8), byte(v)})
 	}
 	return v
 }
@@ -171,6 +243,11 @@ func (d *Decoder) Uint32() uint32 {
 			d.firstError = d.lastError
 		}
 	}
+
+	if d.isDebugMode {
+		d.debugBuf.Write([]byte{byte(v >> 24), byte(v >> 16), byte(v >> 8), byte(v)})
+	}
+
 	return v
 }
 
@@ -189,6 +266,11 @@ func (d *Decoder) Uint64() uint64 {
 			d.firstError = d.lastError
 		}
 	}
+
+	if d.isDebugMode {
+		d.debugBuf.Write([]byte{byte(v >> 56), byte(v >> 48), byte(v >> 40), byte(v >> 32),
+			byte(v >> 24), byte(v >> 16), byte(v >> 8), byte(v)})
+	}
 	return v
 }
 
@@ -206,6 +288,11 @@ func (d *Decoder) Int8() int8 {
 			d.firstError = d.lastError
 		}
 	}
+
+	if d.isDebugMode {
+		d.debugBuf.Write([]byte{byte(v)})
+	}
+
 	return v
 }
 
@@ -223,6 +310,11 @@ func (d *Decoder) Int16() int16 {
 			d.firstError = d.lastError
 		}
 	}
+
+	if d.isDebugMode {
+		d.debugBuf.Write([]byte{byte(v >> 8), byte(v)})
+	}
+
 	return v
 }
 
@@ -241,6 +333,11 @@ func (d *Decoder) Int32() int32 {
 			d.firstError = d.lastError
 		}
 	}
+
+	if d.isDebugMode {
+		d.debugBuf.Write([]byte{byte(v >> 24), byte(v >> 16), byte(v >> 8), byte(v)})
+	}
+
 	return v
 }
 
@@ -259,6 +356,12 @@ func (d *Decoder) Int64() int64 {
 			d.firstError = d.lastError
 		}
 	}
+
+	if d.isDebugMode {
+		d.debugBuf.Write([]byte{byte(v >> 56), byte(v >> 48), byte(v >> 40), byte(v >> 32),
+			byte(v >> 24), byte(v >> 16), byte(v >> 8), byte(v)})
+	}
+
 	return v
 }
 
@@ -277,6 +380,13 @@ func (d *Decoder) Float32() float32 {
 			d.firstError = d.lastError
 		}
 	}
+
+	if d.isDebugMode {
+		// Convert float32 to uint32 bits and write those bytes
+		bits := math.Float32bits(v)
+		d.debugBuf.Write([]byte{byte(bits >> 24), byte(bits >> 16), byte(bits >> 8), byte(bits)})
+	}
+
 	return v
 }
 
@@ -295,5 +405,13 @@ func (d *Decoder) Float64() float64 {
 			d.firstError = d.lastError
 		}
 	}
+
+	if d.isDebugMode {
+		// Convert float64 to uint64 bits and write those bytes
+		bits := math.Float64bits(v)
+		d.debugBuf.Write([]byte{byte(bits >> 56), byte(bits >> 48), byte(bits >> 40), byte(bits >> 32),
+			byte(bits >> 24), byte(bits >> 16), byte(bits >> 8), byte(bits)})
+	}
+
 	return v
 }
